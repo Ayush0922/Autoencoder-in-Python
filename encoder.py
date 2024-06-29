2b1e3299d21701661d40ff36dc4c6564d9a5884e
Ayushman Bhatt <theperfectionist0922@gmail.com>
2024-06-29 09:49:12 +0530

    Create encoder.py
    Setting up important libraries that will be used in code.

---
+++ encoder.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import fashion_mnist
+from keras.models import Model
+from keras.layers import Input, Dense
+from keras.utils import plot_model
