c4feb95d15a694e5199aa722c08291a8ba45a459
Ayushman Bhatt <theperfectionist0922@gmail.com>
2024-07-01 12:45:03 +0530

    Update encoder.py
    Load the data set and normalize the data

--- encoder.py
+++ encoder.py
@@ -4,3 +4,10 @@ from keras.datasets import fashion_mnist
 from keras.models import Model
 from keras.layers import Input, Dense
 from keras.utils import plot_model
+
+# Load Fashion MNIST dataset
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
+
+# Normalize the data
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
