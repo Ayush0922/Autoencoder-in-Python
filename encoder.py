47528032067b9fe35678bc48996fb455d7619fa8
Ayushman Bhatt <theperfectionist0922@gmail.com>
2024-07-01 12:47:59 +0530

    Update encoder.py
    Model definition with 4 dense layers

--- encoder.py
+++ encoder.py
@@ -15,3 +15,17 @@ x_test = x_test.astype('float32') / 255.
 # Flatten the images
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+# Define the autoencoder model
+input_img = Input(shape=(784,))
+encoded = Dense(256, activation='relu')(input_img)
+encoded = Dense(128, activation='relu')(encoded)
+encoded = Dense(64, activation='relu')(encoded)
+encoded = Dense(32, activation='relu')(encoded)
+
+decoded = Dense(64, activation='relu')(encoded)
+decoded = Dense(128, activation='relu')(decoded)
+decoded = Dense(256, activation='relu')(decoded)
+decoded = Dense(784, activation='sigmoid')(decoded)
+
+autoencoder = Model(input_img, decoded)
