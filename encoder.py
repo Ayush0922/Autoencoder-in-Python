ddc6d0b336e9d82c7d836d004604f004adb405c5
Ayushman Bhatt <theperfectionist0922@gmail.com>
2024-07-06 21:54:58 +0530

    Update encoder.py
    done

--- encoder.py
+++ encoder.py
@@ -1,278 +1,67 @@
-import pygame
-import neat
-import os
-import time
-import random
-pygame.font.init()
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.datasets import fashion_mnist
+from keras.models import Model
+from keras.layers import Input, Dense
+from keras.utils import plot_model
 
-WIND_WIDTH = 500
-WIND_HEIGHT = 800
+# Load Fashion MNIST dataset
+(x_train, _), (x_test, _) = fashion_mnist.load_data()
 
-BIRD_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird1.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird2.png"))),pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bird3.png")))]
+# Normalize the data
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
 
-PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","pipe.png")))
+# Flatten the images
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 
-BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","base.png")))
+# Define the autoencoder model
+input_img = Input(shape=(784,))
+encoded = Dense(256, activation='relu')(input_img)
+encoded = Dense(128, activation='relu')(encoded)
+encoded = Dense(64, activation='relu')(encoded)
+encoded = Dense(32, activation='relu')(encoded)
 
-BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bg.png")))
+decoded = Dense(64, activation='relu')(encoded)
+decoded = Dense(128, activation='relu')(decoded)
+decoded = Dense(256, activation='relu')(decoded)
+decoded = Dense(784, activation='sigmoid')(decoded)
 
-STAT_FONT = pygame.font.SysFont("comicsans",50)
+autoencoder = Model(input_img, decoded)
 
-class Bird:
-    IMGS = BIRD_IMGS
-    MAX_ROTATION = 25
-    ROT_VEL = 20
-    ANIMATION_TIME = 5
-    
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
-        self.tilt =0
-        self.tick_count =0
-        self.vel =0
-        self.height = self.y
-        self.img_count = 0
-        self.img = self.IMGS[0]
-    
-    def jump(self):
-        self.vel = -7
-        self.tick_count = 0
-        self.height = self.y
-        
-    def move(self):
-        
-        self.tick_count +=1
-        
-        d = self.vel*self.tick_count + 1.5*self.tick_count**2
-        
-        if d>= 16:
-           d =16
-        
-        if d<0:
-            d -=2
-            
-        self.y = self.y + d
-        
-        if d<0 or self.y<self.height +50:
-            if self.tilt < self.MAX_ROTATION:
-                self.tilt = self.MAX_ROTATION
-        else:
-            if self.tilt > -90:
-                self.tilt -= self.ROT_VEL
-                
-    def draw(self,win):
-        
-        self.img_count+=1
-        
-        if self.img_count< self.ANIMATION_TIME:
-            self.img = self.IMGS[0]
-        elif self.img_count < self.ANIMATION_TIME*2:
-            self.img =self.IMGS[1]
-        elif self.img_count < self.ANIMATION_TIME*3:
-            self.img =self.IMGS[2]
-        elif self.img_count < self.ANIMATION_TIME*4:
-            self.img =self.IMGS[1]
-        elif self.img_count == self.ANIMATION_TIME*4+1:
-            self.img =self.IMGS[0]
-            self.img_count = 0
-            
-        if self.tilt <= -80:
-            self.img = self.IMGS[1]
-            self.img_count = self.ANIMATION_TIME*2
-            
-        rotated_image = pygame.transform.rotate(self.img,self.tilt)
-        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x,self.y)).center)
-        win.blit(rotated_image, new_rect.topleft)
-        
-    def get_mask(self):
-        return pygame.mask.from_surface(self.img)
+# Compile the model
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
-class Pipe:
-    VEL = 3.5
-    
-    def __init__(self,x):
-        self.x = x
-        self.height=0
-        self.GAP = random.randrange(90,200)
-        self.top = 0
-        self.bottom = 0
-        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG,False,True)
-        self.PIPE_BOTTOM = PIPE_IMG
-        self.passed = False
-        self.set_height()
-        
-    def set_height(self):
-        self.height = random.randrange(50,450)
-        self.top = self.height - self.PIPE_TOP.get_height()
-        self.bottom = self.height + self.GAP
-        
-    def move(self):
-        self.x = self.x - self.VEL
-        
-    def draw(self,win):
-        win.blit(self.PIPE_TOP,(self.x,self.top))
-        win.blit(self.PIPE_BOTTOM, (self.x,self.bottom))
-        
-    def collide(self,bird):
-        bird_mask = bird.get_mask()
-        top_mask = pygame.mask.from_surface(self.PIPE_TOP)
-        bottom_mask =  pygame.mask.from_surface(self.PIPE_BOTTOM)
-        top_offset= (self.x -bird.x,self.top-round(bird.y))
-        bottom_offset= (self.x -bird.x,self.bottom-round(bird.y))
-        
-        b_point = bird_mask.overlap(bottom_mask, bottom_offset)
-        t_point = bird_mask.overlap(top_mask, top_offset)
-        
-        if t_point or b_point:
-           return True
-        return False
-class Base:
-    VEL = 3.5
-    WIDTH = BASE_IMG.get_width()
-    IMG = BASE_IMG
-    def __init__(self,y): 
-        self.y = y
-        self.x1 = 0
-        self.x2 = self.WIDTH
-    def move(self):
-        self.x1 -= self.VEL
-        self.x2 -= self.VEL
-        
-        if self.x1 + self.WIDTH <0:
-            self.x1 = self.x2 + self.WIDTH
-        if self.x2 + self.WIDTH < 0:
-           self.x2 = self.x1 + self.WIDTH
-    def draw(self,win):
-        win.blit(self.IMG,(self.x1,self.y))
-        win.blit(self.IMG,(self.x2,self.y))
-    
+# Visualize the architecture of the autoencoder
+plot_model(autoencoder, to_file='autoencoder.png', show_shapes=True, show_layer_names=True)
 
-def draw_window(win,birds,pipes,base,score):
-    win.blit(BG_IMG,(0,0))
-    for pipe in pipes:
-        pipe.draw(win)
-    
-    text = STAT_FONT.render("Score: " + str(score),1,(255,255,255))
-    win.blit(text,(WIND_WIDTH -10 - text.get_width(),10))
-    
-    base.draw(win)
-    for bird in birds:
-        bird.draw(win)
-        pygame.draw.line(win, (255, 0, 0), (245, bird.y+20), (pipe.x, pipe.bottom),3)
-        pygame.draw.line(win, (255, 0, 0), (245, bird.y+20), (pipe.x, pipe.bottom-pipe.GAP),3)
-    pygame.display.update()
+# Train the model
+autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
 
+# Encode and decode some images
+encoded_imgs = autoencoder.predict(x_test)
+decoded_imgs = autoencoder.predict(x_test)
 
-def main(genomes,config):
-    nets = []
-    ge = []
-    birds = []
-    
-    for _,g in genomes:
-        net = neat.nn.FeedForwardNetwork.create(g, config)
-        nets.append(net)
-        birds.append(Bird(230,350))
-        g.fitness = 0
-        ge.append(g)
-    
-    base = Base(700)
-    pipes = [Pipe(600)]
-    win = pygame.display.set_mode((WIND_WIDTH,WIND_HEIGHT))
-    clock = pygame.time.Clock()
-    
-    run = True
-    
-    score = 0
-    
-    while run:
-        clock.tick(60)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run =   False
-                pygame.quit()
-                quit()
-        
-        pipe_ind =0
-        
-        if len(birds) >0:
-            if len(pipes)> 1 and birds[0].x> pipes[0].x + pipes[0].PIPE_TOP.get_width():
-                pipe_ind = 1
-            
-        else: 
-            run = False
-            break
-            
-        for x, bird in enumerate(birds):
-                bird.move()
-                ge[x].fitness += 0.1
-                
-                output = nets[x].activate((bird.y ,abs(bird.y-pipes[pipe_ind].height),abs(bird.y -pipes[pipe_ind].bottom)))
-                
-                if output[0] >0.5:
-                    bird.jump()
-        
-        add_pipe = False
-        rem =[]
-        for pipe in pipes:
-            for x,bird in enumerate(birds):
-                if pipe.collide(bird):
-                    ge[x].fitness -=1
-                    birds.remove(bird)
-                    nets.pop(x)
-                    ge.pop(x)
-                    
-            
-                if not pipe.passed and pipe.x < bird.x:
-                    pipe.passed = True
-                    add_pipe = True
-            
-            if pipe.x +pipe.PIPE_TOP.get_width() <0:
-                rem.append(pipe)
-            
-            pipe.move()
-            
-        if add_pipe:
-            score += 1
-            for g in ge:
-                 g.fitness +=5
-            pipes.append(Pipe(600))
-        for r in rem:
-            pipes.remove(r)
-        for x,bird in enumerate(birds):   
-            if bird.y + bird.img.get_height()>=730 or bird.y < 0:
-                birds.pop(x)
-                nets.pop(x)
-                ge.pop(x)
-            
-            
-            
-        base.move()
-        draw_window(win,birds,pipes,base,score)
-    
-    
-    
+# Display original and reconstructed images
+n = 10
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # Original images
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
 
-def run(config_path):
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
-    p = neat.Population(config)
-    
-    p.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    p.add_reporter(stats)
-    
-    winner = p.run(main,50)
-    
-if __name__ == "__main__":
-    local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir,"config.txt")
-    run(config_path)
-    
-    
-    
-        
-            
-        
-
-            
-      
-
+    # Reconstructed images
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
